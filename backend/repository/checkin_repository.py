e947895bad7dce5582bc02e2ffa983dcca1c75e6
MadaoIsMyBrother <937494107@qq.com>
2021-11-30 20:21:11 -0600

    improve checkin

--- backend/repository/checkin_repository.py
+++ backend/repository/checkin_repository.py
@@ -10,10 +10,10 @@ class CheckinRepository(object):
     def get_checkin_by_userid(self, userid, offset, limit):
         cnx = self.connector.open_connection()
         cursor = cnx.cursor()
-        query = ("SELECT COUNT(*) FROM CheckIn WHERE user_id=%s")
+        query = ("SELECT COUNT(*) FROM CheckIn CheckIn NATURAL JOIN Restaurant r WHERE user_id=%s")
         cursor.execute(query, (userid,))
         total = cursor.fetchone()[0]
-        query = ("SELECT * FROM CheckIn WHERE user_id=%s LIMIT %s OFFSET %s")
+        query = ("SELECT * FROM CheckIn NATURAL JOIN Restaurant r WHERE user_id=%s LIMIT %s OFFSET %s")
         cursor.execute(query, (userid, limit, offset-1))
         profiles = cursor.fetchall()
         row_headers = [x[0] for x in cursor.description]
